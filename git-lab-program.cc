ce7e887dfcb38fb011be2e38e2476a8c534db2cc
Will-56 <willkash56@gmail.com>
2025-01-22 19:58:50 -0500

    new program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Will Kashner
+ *        Date: 1/22/25
+ * 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+}// main
